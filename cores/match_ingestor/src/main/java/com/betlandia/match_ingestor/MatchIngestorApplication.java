bd827529b1fd3c4174f90f47653d848996c54254
portilho13 <junior.portilho2005@gmail.com>
2026-04-18 02:39:53 +0100

    added basic service

---
+++ cores/match_ingestor/src/main/java/com/betlandia/match_ingestor/MatchIngestorApplication.java
@@ -0,0 +1,13 @@
+package com.betlandia.match_ingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchIngestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchIngestorApplication.class, args);
+	}
+
+}
---
+++ cores/match_ingestor/src/test/java/com/betlandia/match_ingestor/MatchIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betlandia.match_ingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchIngestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
