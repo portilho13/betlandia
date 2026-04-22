6c0be2697deac9faa9d3f2dfe3e8cadca4eb1190
portilho13 <junior.portilho2005@gmail.com>
2026-04-22 01:41:09 +0100

    added odds calculator

---
+++ cores/odds_calculator/src/main/java/com/betlandia/odds_calculator/OddsCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.betlandia.odds_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddsCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OddsCalculatorApplication.class, args);
+	}
+
+}
